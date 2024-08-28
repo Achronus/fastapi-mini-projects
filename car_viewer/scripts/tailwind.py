7f04fef05c42e14ed261fb9d666f64b0baca3c6d
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-08-28 14:10:21 +0100

    Refactor tailwind scripts to simplify

--- car_viewer/scripts/tailwind.py
+++ car_viewer/scripts/tailwind.py
@@ -1,8 +1,10 @@
+import os
+from pathlib import Path
 import subprocess
 import argparse
 
 
-def watch(input_file="global.css", output_file="output.css"):
+def watch(input_file="global.css", output_file="styles.css"):
     """
     Start a Tailwind CSS watcher that watches for changes in the input file
     and compiles to the output file.
@@ -23,7 +25,7 @@ def watch(input_file="global.css", output_file="output.css"):
         )
 
 
-def minify(input_file="global.css", output_file="output.min.css"):
+def minify(input_file="global.css", output_file="styles.min.css"):
     """
     Compile and minify the CSS file for production.
 
@@ -45,30 +47,43 @@ def minify(input_file="global.css", output_file="output.min.css"):
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Run Tailwind CSS commands.")
+
+    css_dir = Path(os.getcwd(), "app", "static", "css")
+    input_file = Path(css_dir, "global.css")
+    output_file = Path(css_dir, "styles.css")
+    output_file_min = Path(css_dir, "styles.min.css")
+
     parser.add_argument(
         "command",
         choices=["watch", "minify"],
         help="The command to run: 'watch' or 'minify'.",
     )
+
     parser.add_argument(
         "-i",
         "--input",
-        default="./app/static/css/global.css",
+        default=None,
         help="The input CSS file path.",
     )
     parser.add_argument(
         "-o",
         "--output",
-        default="./app/static/css/output.min.css",
+        default=None,
         help="The output CSS file path.",
     )
 
     args = parser.parse_args()
 
     if args.command == "watch":
-        watch(args.input, args.output)
+        watch(
+            input_file if args.input is None else args.input,
+            output_file if args.output is None else args.output,
+        )
     elif args.command == "minify":
-        minify(args.input, args.output)
+        minify(
+            input_file if args.input is None else args.input,
+            output_file_min if args.output is None else args.output,
+        )
 
 
 if __name__ == "__main__":
